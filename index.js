819999c93c11d42df99dd51695afe395bed2b47e
sumeshi96 <yumapc96@gmail.com>
2022-08-22 09:01:34 +0900

    init commit

---
+++ index.js
@@ -0,0 +1,179 @@
+//HTMLのmyCanvasをJSではcanvasと定義する
+const canvas = document.getElementById("myCanvas");
+
+//2Dで描画するための変数をctxとする
+const ctx = canvas.getContext("2d");
+
+//ボールの初期値
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+//パドルの初期値
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+//ブロックの初期値
+var blockRowCount = 5;
+var blockColumnCount = 3;
+var blockWidth = 75;
+var blockHeight = 20;
+var blockPadding = 10;
+var blockOffsetTop = 30;
+var blockOffsetLeft = 30;
+//スコアとライフの初期値
+var score = 0;
+var lives = 3;
+
+var blocks = [];
+for (var c = 0; c < blockColumnCount; c++) {
+    blocks[c] = [];
+    for (var r = 0; r < blockRowCount; r++) {
+        blocks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+//矢印キーで操作
+function keyDownHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+//マウスでパドルを操作
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth / 2;
+    }
+}
+//衝突判定
+function collisionDetection() {
+    for (var c = 0; c < blockColumnCount; c++) {
+        for (var r = 0; r < blockRowCount; r++) {
+            var b = blocks[c][r];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + blockWidth && y > b.y && y < b.y + blockHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if (score == blockRowCount * blockColumnCount) {
+                        alert("YOU WIN, CONGRATS!");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+}
+//ボールを描画
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}//パドルを描画
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+//ブロックを描画
+function drawblocks() {
+    for (var c = 0; c < blockColumnCount; c++) {
+        for (var r = 0; r < blockRowCount; r++) {
+            if (blocks[c][r].status == 1) {
+                var blockX = (r * (blockWidth + blockPadding)) + blockOffsetLeft;
+                var blockY = (c * (blockHeight + blockPadding)) + blockOffsetTop;
+                blocks[c][r].x = blockX;
+                blocks[c][r].y = blockY;
+                ctx.beginPath();
+                ctx.rect(blockX, blockY, blockWidth, blockHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+//スコアを表示する
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: " + score, 8, 20);
+}
+//ライフを表示する
+function drawLives() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawblocks();
+    drawBall();
+    drawPaddle();
+    drawScore();
+    drawLives();
+    collisionDetection();
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy < ballRadius) {
+        dy = -dy;
+    }
+    else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            lives--;
+            if (!lives) {
+                alert("GAME OVER");
+                document.location.reload();
+            }
+            else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 3;
+                dy = -3;
+                paddleX = (canvas.width - paddleWidth) / 2;
+            }
+        }
+    }
+
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    }
+    else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
+    x += dx;
+    y += dy;
+    requestAnimationFrame(draw);
+}
+
+draw();
